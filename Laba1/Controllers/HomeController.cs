e3d820f63d87be1ea9826f1bd3e5fa9b531b0566
MaksMaksimych <99993879+MaksMaksimych@users.noreply.github.com>
2023-04-09 22:49:16 +0400

    Add files via upload

---
+++ Laba1/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Laba1.Models;
+
+
+
+namespace Laba1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View(new GuestResponse());
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse x, string click)
+        {
+            if (click == "calculate" && ModelState.IsValid)
+            {
+                switch (x.Operation)
+                {
+                    case "+":
+                        x.Result = (decimal)(x.Operand1 + x.Operand2);
+                        break;
+                    case "-":
+                        x.Result = (decimal)(x.Operand1 - x.Operand2);
+                        break;
+                    case "*":
+                        x.Result = (decimal)(x.Operand1 * x.Operand2);
+                        break;
+                    case "/":
+                        x.Result = (decimal)(x.Operand1 / x.Operand2);
+                        break;
+                }
+                HttpCookie cookie = new HttpCookie("OperationInfo");
+                cookie.Value = x.Operand1.ToString() + x.Operation.ToString() + x.Operand2.ToString() + " = " + x.Result.ToString();
+                Response.Cookies.Add(cookie);
+                return View("Thanks", x);
+            }
+            else
+            {
+                if (click == "clear")
+                {
+                    x.Operand1 = 0;
+                    x.Operand2 = 0;
+                    //return View("RsvpForm", x);
+                }
+            }
+            
+
+            ViewBag.ResultSecond = 10;
+            return View(x);
+        }
+
+        public ViewResult Equation()
+        {
+            if (Request.Cookies["OperationInfo"] != null)
+            {
+                string Result = Request.Cookies["OperationInfo"].Value;
+                string ResultInfo = Result.Substring(0, Result.LastIndexOf('=')) + "равно" + Result.Substring(Result.LastIndexOf('=') + 1);
+                ViewBag.Equat = ResultInfo;
+            }
+            return View();
+            
+            
+        }
+    }
+
+    
+}
---
+++ Laba1/Models/GuestResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Laba1.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Укажите первое значение")]
+        public ushort Operand1 { get; set; }
+
+        [Required(ErrorMessage = "Укажите второе значение")]
+        [Compare("Operand1", ErrorMessage = "Значения операндов не совпадают")]
+        public ushort Operand2 { get; set; }
+
+        public string Operation { get; set; }
+
+        public decimal Result { get; set; }
+    }
+
+}
